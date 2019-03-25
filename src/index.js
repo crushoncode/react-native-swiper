0c2dbe51939be59de95c40fcfb5d36e24f57d294
Serina Ko <serinako08@gmail.com>
2019-03-25 11:07:57 +1100

    Remove platform condition

--- src/index.js
+++ src/index.js
@@ -11,8 +11,6 @@ import {
   ScrollView,
   Dimensions,
   TouchableOpacity,
-  ViewPagerAndroid,
-  Platform,
   ActivityIndicator
 } from 'react-native';
 
@@ -289,11 +287,10 @@ export default class extends Component {
     // related to https://github.com/leecade/react-native-swiper/issues/570
     // contentOffset is not working in react 0.48.x so we need to use scrollTo
     // to emulate offset.
-    if (Platform.OS === 'ios') {
-      if (this.initialRender && this.state.total > 1) {
-        this.scrollView.scrollTo({ ...offset, animated: false });
-        this.initialRender = false;
-      }
+
+    if (this.initialRender && this.state.total > 1) {
+      this.scrollView.scrollTo({ ...offset, animated: false });
+      this.initialRender = false;
     }
 
     this.setState(state);
@@ -491,7 +488,7 @@ export default class extends Component {
     });
 
     // trigger onScrollEnd manually in android
-    if (!animated || Platform.OS !== 'ios') {
+    if (!animated) {
       setImmediate(() => {
         this.onScrollEnd({
           nativeEvent: {
